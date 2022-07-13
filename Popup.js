eb2744764c6ab4eda13e2080c024690aae7111df
MadeRyanAryWiguna <ryancholito@gmail.com>
2022-07-13 16:27:16 +0800

    Memperbaharui tampilan web AOV

--- Popup.js
+++ Popup.js
@@ -1,8 +1,16 @@
-let popup = document.getElementById("popup");
+let popup_ID = document.getElementById("popup_ID");
 function openPopup() {
-  popup.classList.add("open-popup");
+  popup_ID.classList.add("open-popup_ID");
 }
 function closePopup() {
-  popup.classList.remove("open-popup");
+  popup_ID.classList.remove("open-popup_ID");
+  I;
+}
+let popup_beli = document.getElementById("popup_beli");
+function openPopup_beli() {
+  popup_beli.classList.add("open-popup_beli");
+}
+function closePopup_beli() {
+  popup_beli.classList.remove("open-popup_beli");
   I;
 }
